48e9510e58285275ed643c29d59d7d1c43a2d8c7
Ratilife <ratilife81@gmail.com>
2024-10-28 16:18:46 +0300

    переписан метод process_image_file

--- litres.py
+++ litres.py
@@ -22,9 +22,7 @@ def determiningFileExtension(image_folder):
 
     return  file_names
 
-
-
-def process_image_file(input_file: str, output_folder: str) -> None:
+def process_image_file2(input_file: str, output_folder: str) -> None:
     """
     Обрабатывает изображение в зависимости от его формата:
     - Если формат JFIF, конвертирует в GIF и сохраняет в указанную папку.
@@ -57,6 +55,40 @@ def process_image_file(input_file: str, output_folder: str) -> None:
             print(f"Файл {input_file} имеет неподдерживаемый формат и был проигнорирован.")
     except Exception as e:
         print(f"Ошибка при обработке файла {input_file}: {e}")
+
+import os
+import shutil
+from PIL import Image
+
+def process_image_file(input_file: str, output_folder: str) -> None:
+    """
+    Обрабатывает изображение в зависимости от его формата:
+    - Если формат JFIF или GIF, конвертирует в PNG и сохраняет в указанную папку.
+    - Игнорирует файлы других форматов.
+
+    :param input_file: Путь к входному файлу изображения.
+    :param output_folder: Путь к папке для сохранения выходного файла.
+    """
+    # Проверка существования папки для сохранения
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Получение имени файла и его расширения
+    filename, file_extension = os.path.splitext(input_file)
+    file_extension = file_extension.lower()
+    output_file = os.path.join(output_folder, os.path.splitext(os.path.basename(input_file))[0] + '.png')
+
+    try:
+        if file_extension in ['.jfif', '.gif']:
+            # Открытие и конвертация JFIF или GIF в PNG
+            with Image.open(input_file) as img:
+                img = img.convert("RGB")  # Преобразование изображения в RGB для PNG
+                img.save(output_file, 'PNG')
+            print(f"Файл {input_file} успешно конвертирован и сохранен как {output_file}")
+        else:
+            print(f"Файл {input_file} имеет неподдерживаемый формат и был проигнорирован.")
+    except Exception as e:
+        print(f"Ошибка при обработке файла {input_file}: {e}")
+
 
 def creatingBook(files,image_folder,output_path="output.pdf"):
     # Создаем объект PDF
